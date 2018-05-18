16103f948e43648e76137e950ba2920dd0ccdbc5
Digo <romero.diego31@gmail.com>
2018-05-18 10:28:00 +0200

    index + affichage

---
+++ affichage.php
@@ -0,0 +1,63 @@
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8" />
+		
+
+
+
+		<title> Affichage</title>
+    </head>
+    
+    <body>
+        <a href="index.html">back</a>
+
+           <?php
+                $server = 'localhost';
+                $login = 'Diego';
+                $mdp = '0';
+                ///Connexion au serveur MySQL
+                try {
+                    $linkpdo = new PDO("mysql:host=$server;dbname=EXO10", $login, $mdp); }
+                catch (Exception $e) {
+                    die('Erreur : ' . $e->getMessage());
+                };
+                $req = $linkpdo->prepare('  SELECT  *
+                                            FROM patient 
+                                            ');
+                $req->execute(array('lsearch' => $_POST['search']));  
+            ?>
+
+       <table>
+        <caption>Résultat recherche</caption>
+        <thead> 
+            <tr>
+                <th>----Civilite----</th>
+                <th>----Nom----</th>
+                <th>----Prenom----</th>
+                <th>----Adresse----</th>
+                <th>----Date naissance----</th>
+                <th>----Lieu naissance----</th>
+                <th>----numero secu----</th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody> 
+            <?php
+            while($donnees = $req->fetch()){
+                echo '<tr>';
+                    echo '<td>'.$donnees['civilite'].'</td>';
+                    echo '<td>'.$donnees['nom'].'</td>';
+                    echo '<td>'.$donnees['prenom'].'</td>';
+                    echo '<td>'.$donnees['adresse'].'</td>';
+                    echo '<td>'.$donnees['date naissance'].'</td>';
+                    echo '<td>'.$donnees['lieu naissance'].'</td>';
+                    echo '<td>'.$donnees['numero secu'].'</td>';
+                    echo '<td>';
+                echo '</tr>';
+            }
+            ?>
+        </tbody>
+    </table>
+    </body>
+</html>
