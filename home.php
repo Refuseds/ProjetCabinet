4a3b66a6e5a79eab9e60a494a1733ca54c5d42d5
SebPou <sebastien.poussard@protonmail.ch>
2018-05-22 19:06:57 +0200

    Connexion

--- home.php
+++ home.php
@@ -1,8 +1,33 @@
 <HTML>
 <?php
 
-if ( isset($_POST['nom'])) {
-	$cookieName = $_POST['nom'];
-	setcookie('cNom', $cookieName, time() + 3600);
-	setcookie('cCpt', $cookieCpt, time() + 3600);
+if ( empty($_POST['id']) || empty($_POST['pw']) ) { 
+	setcookie('id', 'login_incomplet', time() + (86400 * 30));
+	setcookie('pw', 'login_incomplet', time() + (86400 * 30));
+	header('Location: index.php');	
+} else {
+	
+	try {
+		$linkpdo = new PDO("mysql:host=localhost;dbname=cabinet", 'root', '');
+	} catch (Exception $e) {
+	die('Erreur : ' . $e->getMessage());
+	}
+	
+// preparation de la requete 
+	$req = $linkpdo->prepare("SELECT * FROM login WHERE identifiant = :id and password = :pw ");
+
+//Exécution de la requête avec les paramètres passés sous forme de tableau indexé
+	$req->execute(array('id'=>$_POST['id'], 'pw'=>$_POST['pw']) );
+	$data = $req->fetch();
+	if ($data == false ) {
+		setcookie('id', 'login_erreur', time() + (86400 * 30));
+		setcookie('pw', 'login_erreur', time() + (86400 * 30));
+		//header('Location: index.php');
+		echo "test";
+		var_dump($_COOKIE['id']);
+	}
+	setcookie('id', $_POST['id'], time() + (86400 * 30));
+	setcookie('pw', $_POST['pw'], time() + (86400 * 30));
 }
+?>
+
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <HTML>
-
-<form method="post" action="home.php">
+<a> Veuillez vous identifier </a>
+<form method="POST" action="home.php">
 	<p> identifiant :
 		<input type="text" name="id">
 		mot de passe :
@@ -8,6 +8,18 @@
 	<input type="submit" name="envoyer" value="se connecter">
 	</p>
 </form>
+<br>
+<?php
+	if( isset($_COOKIE['id']) ) {
+		if ( $_COOKIE['id'] == 'login_incomplet' || $_COOKIE['pw'] == 'login_incomplet' ) {
+			echo "<a> Identification invalide ( apparait si aucun champs n'a été renseigné )  </a> ";
+		}
+		if ( $_COOKIE['id'] == 'login_erreur' ||  $_COOKIE['pw'] == 'login_erreur' ) {
+			echo "<a> Le nom d'utilisateur ou le mot de passe est invalide ( apparait qd login incorrect </a>";
+		}
+	}
+	var_dump($_COOKIE['id']);
+?>
 
 
 </HTML>
