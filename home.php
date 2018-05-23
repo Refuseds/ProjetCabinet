6f22e70fcfcf820e921c23e70611eaf8af8151f2
SebPou <sebastien.poussard@protonmail.ch>
2018-05-23 15:50:55 +0200

    identification via session securisée

--- home.php
+++ home.php
@@ -1,32 +1,58 @@
-<HTML>
 <?php
+session_start();
 
-if ( empty($_POST['id']) || empty($_POST['pw']) ) { 
-	setcookie('id', 'login_incomplet', time() + (86400 * 30));
-	setcookie('pw', 'login_incomplet', time() + (86400 * 30));
-	header('Location: index.php');	
-} else {
-	
-	try {
-		$linkpdo = new PDO("mysql:host=localhost;dbname=cabinet", 'root', '');
-	} catch (Exception $e) {
-	die('Erreur : ' . $e->getMessage());
-	}
-	
-// preparation de la requete 
-	$req = $linkpdo->prepare("SELECT * FROM login WHERE identifiant = :id and password = :pw ");
 
-//Exécution de la requête avec les paramètres passés sous forme de tableau indexé
-	$req->execute(array('id'=>$_POST['id'], 'pw'=>$_POST['pw']) );
-	$data = $req->fetch();
-	if ($data == false ) {
-		setcookie('id', 'login_erreur', time() + (86400 * 30));
-		setcookie('pw', 'login_erreur', time() + (86400 * 30));
-		header('Location: index.php');
-	} else {
-	setcookie('id', $_POST['id'], time() + (86400 * 30));
-	setcookie('pw', $_POST['pw'], time() + (86400 * 30));
+	if ( $_SESSION['login'] != 'ok' ) {
+		if ( (empty($_POST['id']) || empty($_POST['pw']) ) ) { 
+		$_SESSION["login"] = 'incomplet';
+		header('Location: index.php');	
+		} else {
+		
+		// connexion à la BDD
+		try {
+			$linkpdo = new PDO("mysql:host=localhost;dbname=cabinet", 'root', '');
+		} catch (Exception $e) {
+		die('Erreur : ' . $e->getMessage());
+		}
+		
+		// preparation de la requete 
+		$req = $linkpdo->prepare("SELECT password FROM login WHERE identifiant = :id");
+
+		//Exécution de la requête avec les paramètres passés sous forme de tableau indexé
+		$req->execute(array('id'=>$_POST['id'] ));
+		$data = $req->fetch();
+		// si requete fausse redirection sinon creation de session correcte
+		var_dump($data);
+		if ($data == false ) {
+			$_SESSION["login"] = 'incorrect';
+			header('Location: index.php');
+		} else {
+			if ( password_verify($_POST['pw'], $data[0]) ) {
+				$_SESSION["login"] = 'ok';
+			} else {
+				header('Location: index.php');
+			}
+			
+		}
+		}	
 	}
+// si l'utilisateur à cliqué sur deconnexion on detruit la session et renvoi sur la page de connexion
+if ( isset($_POST['deconnexion']) ) {
+	session_destroy();
+	header('Location: index.php');
 }
+$options = [
+    'cost' => 12,
+];
+
+// fonction pour crypter les mot de passe
+// $hash = password_hash("admin", PASSWORD_BCRYPT, $options);
 ?>
+<HTML>
+<form method="POST" action="home.php">
+		<input type="submit" name="deconnexion" value="se deconnecter">
+	</p>
+</form>
+
+
 
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+session_start();
+?>
 <HTML>
 <a> Veuillez vous identifier </a>
 <form method="POST" action="home.php">
@@ -5,19 +8,27 @@
 		<input type="text" name="id">
 		mot de passe :
 		<input type="text" name="pw">
-	<input type="submit" name="envoyer" value="se connecter">
+		<input type="submit" name="envoyer" value="se connecter">
+		<input type="hidden" value="true" name="tentative" />
 	</p>
 </form>
 <br>
+
 <?php
-	if( isset($_COOKIE['id']) ) {
-		if ( $_COOKIE['id'] == 'login_incomplet' || $_COOKIE['pw'] == 'login_incomplet' ) {
-			echo "<a> Identification invalide ( apparait si aucun champs n'a été renseigné )  </a> ";
-		}
-		if ( $_COOKIE['id'] == 'login_erreur' ||  $_COOKIE['pw'] == 'login_erreur' ) {
-			echo "<a> Le nom d'utilisateur ou le mot de passe est invalide ( apparait qd login incorrect ) </a>";
-		}
+
+// test l'état de la session pour modifier l'affichage et si deja connecté rediriger vers home.php
+if( isset($_SESSION['login']) ) {
+	if ( $_SESSION['login'] == 'ok' ) {
+		header('Location: home.php');
 	}
+	if ( $_SESSION['login'] == 'incomplet' ) {
+		echo "<a> Identification invalide ( apparait si aucun champs n'a été renseigné )  </a> ";
+	}
+	if ( $_SESSION['login'] == 'incorrect') {
+		echo "<a> Le nom d'utilisateur ou le mot de passe est invalide ( apparait qd login incorrect ) </a>";
+	}
+}
+
 ?>
 
 
