5b3846a8b9b6efa4bc85cf7d6c68c61ea58103b0
SebPou <sebastien.poussard@protonmail.ch>
2018-05-28 13:38:04 +0200

    icone application

--- menu.php
+++ menu.php
@@ -4,7 +4,9 @@
     <script src="/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Cabinet Medical</a>
+  <a class="navbar-brand" href="#">
+    <img src="/img/medical.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    Cabinet Medical</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
