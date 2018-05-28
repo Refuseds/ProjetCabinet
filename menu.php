b83b5c4d19d6ba731873bfbc3aa41b82c8b1fb16
SebPou <sebastien.poussard@protonmail.ch>
2018-05-28 10:53:21 +0200

    menu reactif

--- menu.php
+++ menu.php
@@ -1,7 +1,24 @@
 <head>
     <link rel="stylesheet" type="text/css" href="/css/header.css">
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+
 
 </head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Cabinet Medical</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link <?php if ( (substr($_SERVER['REQUEST_URI'], 1, 7)) == "medecin" ) { echo "active";}?> " href="/medecin/affichage.php">Medecins <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link <?php if ( (substr($_SERVER['REQUEST_URI'], 1, 7)) == "patient" ) { echo "active";}?>" href="/patient/affichage.php">Patients</a>
+      <a class="nav-item nav-link <?php if ( (substr($_SERVER['REQUEST_URI'], 1, 3)) == "rdv" ) { echo "active";}?>" href="/rdv/affichage.php">RDV</a>
+      <a class="nav-item nav-link <?php if ( (substr($_SERVER['REQUEST_URI'], 1, 11)) == "patient" ) { echo "statistique";}?>" href="#">Statistiques</a>
+    </div>
+  </div>
+</nav>
 <nav>
     <ul id="banniere-menu">
         <li><a href="/patient/affichage.php">Usagers</a></li>
@@ -10,3 +27,5 @@
         <li><a href="/stat/statistiques.php">Statistiques</a></li>
     </ul>
 </nav>
+<?php
+ ?>
