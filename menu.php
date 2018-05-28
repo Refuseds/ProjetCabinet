a22a0213164c92b1462cc744b8538be8a8f5d848
SebPou <sebastien.poussard@protonmail.ch>
2018-05-28 13:29:29 +0200

    se deconnecter

--- menu.php
+++ menu.php
@@ -2,8 +2,6 @@
     <link rel="stylesheet" type="text/css" href="/css/header.css">
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
     <script src="/bootstrap/js/bootstrap.min.js"></script>
-
-
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Cabinet Medical</a>
@@ -17,6 +15,13 @@
       <a class="nav-item nav-link <?php if ( (substr($_SERVER['REQUEST_URI'], 1, 3)) == "rdv" ) { echo "active";}?>" href="/rdv/affichage.php">RDV</a>
       <a class="nav-item nav-link <?php if ( (substr($_SERVER['REQUEST_URI'], 1, 11)) == "patient" ) { echo "statistique";}?>" href="#">Statistiques</a>
     </div>
+    <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <form method="POST" action="../home.php">
+                <button type="submit" name="deconnexion" class="btn btn-light">se déconnecter</button>
+              </form>
+            </li>
+    </ul>
   </div>
 </nav>
 <?php
