5d172e1f931a058c48ccfe9373c40b38f570ec75
SebPou <sebastien.poussard@protonmail.ch>
2018-05-30 15:50:35 +0200

    modale patient & medecin
    Suppression OK
    Modificatin en cours

--- medecin/affichage.php
+++ medecin/affichage.php
@@ -1,42 +1,55 @@
 <?php include '../secure.php';?>
 <?php
-    if( isset($_POST['valid'])){
-        $server = 'localhost';
-        $login = 'root';
-        $mdp = 'root';
-        try {
-            $linkpdo = new PDO("mysql:host=$server;dbname=cabinet", $login, $mdp);
-        }
-        catch (Exception $e) {
-            die('Erreur : ' . $e->getMessage());
-        };
-
-        if($_POST['civilite'] == "Mme."){
-            $civ = 0;
-        }else if($_POST['civilite'] == "Mr."){
-            $civ = 1;
-        }else if($_POST['civilite'] == ""){
-            echo " Champ civilité non renseigné";
-        }
-
-        $req = $linkpdo->prepare('INSERT INTO medecin (
-                                        civilite,
-                                        nom,
-                                        prenom
-                                      )
-                                VALUES(
-                                    :lcivilite,
-                                    :lnom,
-                                    :lprenom
-                                  )
-                            ');
-        $req->execute(array(
-                            'lcivilite' => $_POST['civilite'],
-                            'lnom' => $_POST['nom'],
-                            'lprenom' => $_POST['prenom']
-        ));
-        //print_r($req->errorInfo());
+if( isset($_POST['valid'])){
+    $server = 'localhost';
+    $login = 'root';
+    $mdp = 'root';
+    try {
+        $linkpdo = new PDO("mysql:host=$server;dbname=cabinet", $login, $mdp);
     }
+    catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    };
+
+    if($_POST['civilite'] == "Mme."){
+        $civ = 0;
+    }else if($_POST['civilite'] == "Mr."){
+        $civ = 1;
+    }else if($_POST['civilite'] == ""){
+        echo " Champ civilité non renseigné";
+    }
+
+    $req = $linkpdo->prepare('INSERT INTO medecin (
+                                    civilite,
+                                    nom,
+                                    prenom
+                                  )
+                            VALUES(
+                                :lcivilite,
+                                :lnom,
+                                :lprenom
+                              )
+                        ');
+    $req->execute(array(
+                        'lcivilite' => $_POST['civilite'],
+                        'lnom' => $_POST['nom'],
+                        'lprenom' => $_POST['prenom']
+    ));
+}
+if( isset($_POST['suppression'])) {
+  $server = 'localhost';
+  $login = 'root';
+  $mdp = 'root';
+  try {
+      $linkpdo = new PDO("mysql:host=$server;dbname=cabinet", $login, $mdp);
+  }
+  catch (Exception $e) {
+      die('Erreur : ' . $e->getMessage());
+  };
+
+  $req = $linkpdo->prepare('DELETE FROM  medecin WHERE pkmedecin = :pk');
+  $req->execute(array('pk' => $_POST['pkmedecin'],));
+}
 ?>
 <html lang="fr">
 <head>
@@ -75,9 +88,10 @@
 							<th>Modification / suppression</th>
 						</tr>
 					</thead>
-					<tbody>
+          <tbody>
 						<?php
 						while($donnees = $req->fetch()){
+              // construction des données du tableau
 							echo '<tr>';
 							if ( $donnees['civilite'] == '1' ) {
 								echo '<td>Homme</td>';
@@ -86,22 +100,87 @@
 							}
 							echo '<td>'.$donnees['nom'].'</td>';
 							echo '<td>'.$donnees['prenom'].'</td>';
-							echo '<td>'.'<a href="modification.php?id='.$donnees['pkmedecin'].' ">
-							<img  href="modification.php?id='.$donnees['pkmedecin'].' "src="/img/wrench.png">
-								</a>'.'<a>&emsp;&emsp;&emsp;</a>'.'<a href="suppression.php?id='.$donnees['pkmedecin'].' ">
-								<img  href="modification.php?id='.$donnees['pkmedecin'].' "src="/img/trash.png">
-									</a>'.'</td>';
 
-						}
-						?>
+              // bouton pointant la modale de modification
+							echo '<td>'.'<a data-toggle="modal" data-target="#modifier'.$donnees['pkmedecin'].'"><img src="/img/wrench.png"></a>';
+
+              // modal de mofification
+							?>
+							<div class="modal fade" id="modifier<?php echo $donnees['pkmedecin'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+								<div class="modal-dialog" role="document">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h5 class="modal-title" id="exampleModalLabel">Modifier un medecin</h5>
+											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+											</button>
+										</div>
+										<div class="modal-body">
+											<form action="affichage.php" method="post">
+												<div class="form-group row">
+													<label class="col-sm-4 col-form-label">Civilité</label>
+													<div class="col-sm-8">
+														<select class="custom-select mr-sm-2" name="civilite" >
+															<option <?php if ($donnees['civilite'] == 1 ) {echo 'selected ';} ?> value="1">Mr.</option>
+															<option <?php if ($donnees['civilite'] == 0 ) {echo 'selected ';} ?> value="0">Mme</option>
+														</select>
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-sm-4 col-form-label">Nom</label>
+													<div class="col-sm-8">
+														<input type="text" value="<?php echo $donnees['nom'];?>" class="form-control" name="nom">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-sm-4 col-form-label">Prénom</label>
+													<div class="col-sm-8">
+														<input type="text" value="<?php echo $donnees['prenom'];?>" class="form-control" name="prenom">
+													</div>
+												</div>
+												<br>
+												<input class="btn btn-success float-right" type="submit" value="Valider" name="valid">
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+							<?php
+							// espace entre les deux icones modifier et supprimer
+							echo '<a>&emsp;&emsp;&emsp;</a>';
+							// bouton pointant la modale de suppression
+							echo '<a data-toggle="modal" data-target="#supprimer'.$donnees['pkmedecin'].'"><img src="/img/trash.png"></a></td>';
+							// modal de Suppression
+							?>
+							<div class="modal fade" id="supprimer<?php echo $donnees['pkmedecin'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      					<div class="modal-dialog" role="document">
+      						<div class="modal-content">
+      							<div class="modal-header">
+      								<h5 class="modal-title" id="exampleModalLabel">Confirmation de Suppression</h5>
+      								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      									<span aria-hidden="true">&times;</span>
+      								</button>
+      							</div>
+      							<div class="modal-body">
+                    <br>
+                    <a> Êtes-vous sûr de vouloir supprimer le patient  <b>'<?php echo $donnees['prenom'].' '.$donnees['nom'];?></b>  ?
+      								<form action="affichage.php" method="post">
+      									<br>
+                        <input type="hidden" value="<?php echo $donnees['pkmedecin'];?>" name="pkmedecin" />
+      									<input class="btn btn-outline-danger float-right" type="submit" value="Supprimer" name="suppression">
+      							</form>
+      						</div>
+      					</div>
+      				</div>
+						<?php } ?>
 					</tbody>
 				</table>
+        <!-- Bouton pointant vers la modale d'ajout de medecin -->
 				<button type="button" class="btn btn-success float-right ajouter" data-toggle="modal" data-target="#ajout">
 					Ajouter un médecin
 				</button>
 			</div>
-
-				<!-- Modal -->
+				<!-- Modal d'ajout d'un medecin  -->
 				<div class="modal fade" id="ajout" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 					<div class="modal-dialog" role="document">
 						<div class="modal-content">
--- patient/affichage.php
+++ patient/affichage.php
@@ -1,5 +1,6 @@
 <?php include '../secure.php';?>
 <?php
+// ajout de données
 if( isset($_POST['valid'])){
 	$server = 'localhost';
 	$login = 'root';
@@ -39,6 +40,21 @@ if( isset($_POST['valid'])){
 		'llieunaissance' => $_POST['lieunaissance'],
 		'lnumsecurite' => $_POST['numsecurite']
 	));
+}
+
+if ( isset($_POST['suppression'])) {
+	$server = 'localhost';
+	$login = 'root';
+	$mdp = 'root';
+	try {
+		$linkpdo = new PDO("mysql:host=$server;dbname=cabinet", $login, $mdp);
+	}
+	catch (Exception $e) {
+		die('Erreur : ' . $e->getMessage());
+	};
+
+	$req = $linkpdo->prepare('DELETE FROM patient WHERE pkpatient = :pk');
+	$req->execute(array('pk' => $_POST['pkpatient'] ));
 }
 ?>
 <html lang="fr">
@@ -86,6 +102,7 @@ if( isset($_POST['valid'])){
 					</thead>
 					<tbody>
 						<?php
+						// construction des données du tableau
 						while($donnees = $req->fetch()){
 							echo '<tr>';
 							if ( $donnees['civilite'] == '1' ) {
@@ -99,22 +116,111 @@ if( isset($_POST['valid'])){
 							echo '<td>'.$donnees['datenaissance'].'</td>';
 							echo '<td>'.$donnees['lieunaissance'].'</td>';
 							echo '<td>'.$donnees['numsecurite'].'</td>';
-							echo '<td>'.'<a href="modification.php?id='.$donnees['pkpatient'].' ">
-							<img  href="modification.php?id='.$donnees['pkpatient'].' "src="/img/wrench.png">
-							</a>'.'<a>&emsp;&emsp;&emsp;</a>'.'<a href="suppression.php?id='.$donnees['pkpatient'].' ">
-							<img  href="modification.php?id='.$donnees['pkpatient'].' "src="/img/trash.png">
-							</a>'.'</td>';
-						}
-						?>
+							// bouton pointant la modale de modification
+							echo '<td>'.'<a data-toggle="modal" data-target="#modifier'.$donnees['pkpatient'].'"><img src="/img/wrench.png"></a>';
+
+							// modal de mofification
+							?>
+							<div class="modal fade" id="modifier<?php echo $donnees['pkpatient'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+								<div class="modal-dialog" role="document">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h5 class="modal-title" id="exampleModalLabel">Modifier un patient</h5>
+											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+											</button>
+										</div>
+										<div class="modal-body">
+											<form action="affichage.php" method="post">
+												<div class="form-group row">
+													<label class="col-sm-4 col-form-label">Civilité</label>
+													<div class="col-sm-8">
+														<select class="custom-select mr-sm-2" name="civilite" >
+															<option <?php if ($donnees['civilite'] == 1 ) {echo 'selected ';} ?> value="1">Mr.</option>
+															<option <?php if ($donnees['civilite'] == 0 ) {echo 'selected ';} ?> value="0">Mme</option>
+														</select>
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-sm-4 col-form-label">Nom</label>
+													<div class="col-sm-8">
+														<input type="text" value="<?php echo $donnees['nom'];?>" class="form-control" name="nom">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-sm-4 col-form-label">Prénom</label>
+													<div class="col-sm-8">
+														<input type="text" value="<?php echo $donnees['prenom'];?>" class="form-control" name="prenom">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-sm-4 col-form-label">Adresse</label>
+													<div class="col-sm-8">
+														<input type="text" value="<?php echo $donnees['adresse'];?>" class="form-control" name="adresse">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-sm-4 col-form-label">Date de naissance</label>
+													<div class="col-sm-8">
+														<input type="date" value="<?php echo $donnees['datenaissance'];?>" class="form-control" name="datenaissance">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-sm-4 col-form-label">Lieu de naissance</label>
+													<div class="col-sm-8">
+														<input type="text" value="<?php echo $donnees['lieunaissance'];?>" class="form-control" name="lieunaissance">
+													</div>
+												</div>
+												<div class="form-group row">
+													<label class="col-sm-4 col-form-label">N° sécurité sociale</label>
+													<div class="col-sm-8">
+														<input type="text" value="<?php echo $donnees['numsecurite'];?>" class="form-control" name="numsecurite">
+													</div>
+												</div>
+												<br>
+												<input class="btn btn-success float-right" type="submit" value="Valider" name="valid">
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+							<?php
+							// espace entre les deux icones modifier et supprimer
+							echo '<a>&emsp;&emsp;&emsp;</a>';
+							// bouton pointant la modale de suppression
+							echo '<a data-toggle="modal" data-target="#supprimer'.$donnees['pkpatient'].'"><img src="/img/trash.png"></a></td>';
+							// modal de Suppression
+							?>
+							<div class="modal fade" id="supprimer<?php echo $donnees['pkpatient'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      					<div class="modal-dialog" role="document">
+      						<div class="modal-content">
+      							<div class="modal-header">
+      								<h5 class="modal-title" id="exampleModalLabel">Confirmation de Suppression</h5>
+      								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+      									<span aria-hidden="true">&times;</span>
+      								</button>
+      							</div>
+      							<div class="modal-body">
+                    <br>
+                    <a> Êtes-vous sûr de vouloir supprimer le patient  <b>'<?php echo $donnees['prenom'].' '.$donnees['nom'];?></b>  ?
+      								<form action="affichage.php" method="post">
+      									<br>
+                        <input type="hidden" value="<?php echo $donnees['pkpatient'];?>" name="pkpatient" />
+      									<input class="btn btn-outline-danger float-right" type="submit" value="Supprimer" name="suppression">
+      							</form>
+      						</div>
+      					</div>
+      				</div>
+						<?php } ?>
 					</tbody>
 				</table>
-			<!-- Button trigger modal -->
+			<!-- Boutton pointant vers la modale d'ajout -->
 			<button type="button" class="btn btn-success float-right ajouter" data-toggle="modal" data-target="#ajout">
 				Ajouter un patient
 			</button>
 		</div>
 
-			<!-- Modal -->
+			<!-- Modale d'ajout -->
 			<div class="modal fade" id="ajout" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 				<div class="modal-dialog" role="document">
 					<div class="modal-content">
@@ -178,6 +284,5 @@ if( isset($_POST['valid'])){
 					</div>
 				</div>
 			</div>
-		</div>
-	</body>
-	</html>
+		</body>
+</html>
